567cc35f529025db41a65fdcb194a88f71801b1b
JensWendt <92271654+JensWendt@users.noreply.github.com>
2026-02-06 14:06:07 +0100

    included edge cases, fixed bugs

--- SetPixelSize.py
+++ SetPixelSize.py
@@ -25,6 +25,11 @@ from omero.rtypes import rstring, rlong
 import omero.scripts as scripts
 from omero.model.enums import UnitsLength
 
+PARAM_PIXEL_SIZE_X = "Pixel_Size_[X]"
+PARAM_PIXEL_SIZE_Y = "Pixel_Size_[Y]_(Only_set_if_different_from_X)"
+PARAM_PIXEL_SIZE_Z = "Pixel_Size_[Z]_(Only_set_if_Z_Dimension_exists)"
+
+
 def get_images(conn, script_params):
     # returns a list of images
     data_type = script_params["Data_Type"]
@@ -88,14 +93,20 @@ def set_pixel_value(conn, script_params):
     numberOfImages = len(images)
     counter = 0
     overwrite = script_params["Overwrite_existing_values?"]
+    pixelSizeX = None
+    pixelSizeY = None
+    pixelSizeZ = None
     for image in images:
         unit = get_unit(script_params)
-        sizeX = script_params["Pixel_Size_[X]"]
-        sizeY = script_params["Pixel_Size_[Y]"]
-        sizeZ = script_params["Pixel_Size_[Z]"]
-        pixelSizeX = omero.model.LengthI(sizeX, unit)
-        pixelSizeY = omero.model.LengthI(sizeY, unit)
-        pixelSizeZ = omero.model.LengthI(sizeZ, unit)
+        if PARAM_PIXEL_SIZE_X in script_params:
+            sizeX = script_params[PARAM_PIXEL_SIZE_X]
+            pixelSizeX = omero.model.LengthI(sizeX, unit)
+        if PARAM_PIXEL_SIZE_Y in script_params:
+            sizeY = script_params[PARAM_PIXEL_SIZE_Y]
+            pixelSizeY = omero.model.LengthI(sizeY, unit)
+        if PARAM_PIXEL_SIZE_Z in script_params:
+            sizeZ = script_params[PARAM_PIXEL_SIZE_Z]
+            pixelSizeZ = omero.model.LengthI(sizeZ, unit)
         
         # check if pixel size already exists
         pixels = image.getPrimaryPixels()._obj
@@ -105,38 +116,66 @@ def set_pixel_value(conn, script_params):
 
         # Logic to set the new values
         # checks size of Z dimension as indicator if Z pixel size should be set
+        internal_counter = 0
         if overwrite:
-            if existingSizeX:
+
+            if existingSizeX and pixelSizeX:
                 print(f"Overwriting existing Pixel Size X for Image ID {image.getId()}: "
-                      f"{existingSizeX.getValue()} {existingSizeX.getUnit()}")
+                      f"{existingSizeX.getValue()} {existingSizeX.getUnit()} ==> "
+                      f"{pixelSizeX.getValue()} {pixelSizeX.getUnit()}")
                 pixels.setPhysicalSizeX(pixelSizeX)
-            else:
+                internal_counter += 1
+            elif pixelSizeX:
                 pixels.setPhysicalSizeX(pixelSizeX)
-            if existingSizeY:
+                internal_counter += 1
+
+            if existingSizeY and pixelSizeY:
                 print(f"Overwriting existing Pixel Size Y for Image ID {image.getId()}: "
-                      f"{existingSizeY.getValue()} {existingSizeY.getUnit()}")
+                      f"{existingSizeY.getValue()} {existingSizeY.getUnit()} ==> "
+                      f"{pixelSizeY.getValue()} {pixelSizeY.getUnit()}")
                 pixels.setPhysicalSizeY(pixelSizeY)
-            else:
+                internal_counter += 1
+            elif pixelSizeY:
                 pixels.setPhysicalSizeY(pixelSizeY)
-            if pixels.getSizeZ()>1:
-                if existingSizeZ:
+                internal_counter += 1
+
+            if pixels.getSizeZ().getValue()>1:
+                if existingSizeZ and pixelSizeZ:
                     print(f"Overwriting existing Pixel Size Z for Image ID {image.getId()}: "
-                          f"{existingSizeZ.getValue()} {existingSizeZ.getUnit()}")
+                          f"{existingSizeZ.getValue()} {existingSizeZ.getUnit()} ==> "
+                          f"{pixelSizeZ.getValue()} {pixelSizeZ.getUnit()}")
                     pixels.setPhysicalSizeZ(pixelSizeZ)
-                else:
+                    internal_counter += 1
+                elif pixelSizeZ:
                     pixels.setPhysicalSizeZ(pixelSizeZ)
+                    internal_counter += 1
         else:
-            if not existingSizeX:
+            if not existingSizeX and pixelSizeX:
                 pixels.setPhysicalSizeX(pixelSizeX)
-            if not existingSizeY:
+                internal_counter += 1
+            elif existingSizeX and pixelSizeX:
+                print(f"Image ID {image.getId()} already has Pixel Size X: "
+                      f"{existingSizeX.getValue()} {existingSizeX.getUnit()}. Skipping setting new value.")
+                
+            if not existingSizeY and pixelSizeY:
                 pixels.setPhysicalSizeY(pixelSizeY)
-            if pixels.getSizeZ()>1 and not existingSizeZ:
+                internal_counter += 1
+            elif existingSizeY and pixelSizeY:
+                print(f"Image ID {image.getId()} already has Pixel Size Y: "
+                      f"{existingSizeY.getValue()} {existingSizeY.getUnit()}. Skipping setting new value.")
+                
+            if pixels.getSizeZ().getValue()>1 and not existingSizeZ and pixelSizeZ:
                 pixels.setPhysicalSizeZ(pixelSizeZ)
-
-        # we are gonna save regardless if we set new values or not to make it easier
-        conn.getUpdateService().saveObject(pixels)
-        counter += 1
-
+                internal_counter += 1
+            elif pixels.getSizeZ().getValue()>1 and existingSizeZ and pixelSizeZ: 
+                print(f"Image ID {image.getId()} already has Pixel Size Z: "
+                      f"{existingSizeZ.getValue()} {existingSizeZ.getUnit()}. Skipping setting new value.")
+        
+        # if any new pixel value was set we count this image as updated
+        # and save the changes to the database
+        if internal_counter > 0: 
+            conn.getUpdateService().saveObject(pixels)
+            counter += 1
     
     return numberOfImages, counter
 
@@ -148,7 +187,11 @@ def run_script():
     client = scripts.client(
         'Set_Pixelsize',
         """
-    This script sets the pixel size for one/multiple Images.
+    This script sets the pixel size for one/multiple Images.\n
+    Works for single Images or all Images in a Dataset, Plate, Screen or Project.\n
+    Default behavior is to overwrite the existing pixel size values, but you can
+    also choose to only set the pixel size for Images where this information is missing.
+    If you provide a value for pixel size Z and the image does not have a Z dimension this will be ignored.
         """,
         scripts.String(
             "Data_Type", optional=False, grouping="1",
@@ -161,16 +204,16 @@ def run_script():
             "\nYou can provide multiple IDs separated with a ','.").ofType(rlong(0)),
 
         scripts.Float(
-            "Pixel_Size_[X]", optional=False, grouping="3",
+            PARAM_PIXEL_SIZE_X, optional=True, grouping="3",
             description="Size of the pixels, e.g. 0.0025"),
 
         scripts.Float(
-            "Pixel_Size_[Y]_(Only_set_if_different_from_X)", optional=True, grouping="4",
+            PARAM_PIXEL_SIZE_Y, optional=True, grouping="4",
             description="If left empty the value for Pixel Size [X] is used. "
             "Size of the pixels, e.g. 0.0025"),
 
         scripts.Float(
-            "Pixel_Size_[Z]_(Only_set_if_Z_Dimension_exists)", optional=True, grouping="5",
+            PARAM_PIXEL_SIZE_Z, optional=True, grouping="5",
             description="If left empty and the image has Z Dimension "
             "the value for Pixel Size [X] is used. Size of the pixels, e.g. 0.0025"),
 
@@ -193,31 +236,35 @@ def run_script():
         # process the list of args above.
         script_params = {}
         for key in client.getInputKeys():
-            # for the two optional pixel size inputs we need to check if they are empty 
-            # and if yes use the value from Pixel_Size_[X] and shorten the name
-            if key == "Pixel_Size_[Y]_(Only_set_if_different_from_X)":
-                if client.getInput(key):
-                    script_params["Pixel_Size_[Y]"] = client.getInput(key, unwrap=True)
-                else:
-                    script_params["Pixel_Size_[Y]"] = client.getInput("Pixel_Size_[X]", unwrap=True)
-            elif key == "Pixel_Size_[Z]_(Only_set_if_Z_Dimension_exists)":
-                if client.getInput(key):
-                    script_params["Pixel_Size_[Z]"] = client.getInput(key, unwrap=True)
-                else:
-                    script_params["Pixel_Size_[Z]"] = client.getInput("Pixel_Size_[X]", unwrap=True)
-            else: 
-                if client.getInput(key):
-                    script_params[key] = client.getInput(key, unwrap=True)
+            if client.getInput(key):
+                script_params[key] = client.getInput(key, unwrap=True)
+
+        # if the optional pixel size inputs are empty we set them to the value of Pixel_Size_[X]
+        if PARAM_PIXEL_SIZE_Y not in script_params and PARAM_PIXEL_SIZE_X in script_params:
+            script_params[PARAM_PIXEL_SIZE_Y] = script_params[PARAM_PIXEL_SIZE_X]
+        if PARAM_PIXEL_SIZE_Z not in script_params and PARAM_PIXEL_SIZE_X in script_params:
+            script_params[PARAM_PIXEL_SIZE_Z] = script_params[PARAM_PIXEL_SIZE_X]
 
         # wrap client to use the Blitz Gateway
         conn = BlitzGateway(client_obj=client)
         print("script params")
         for k, v in script_params.items():
             print(k, v)
-        numberOfImages, counter = set_pixel_value(conn, script_params)
-        message = f"Set the pixel size to {script_params['Pixel_Size_[X]']} {script_params['Unit']} for {counter} of {numberOfImages} Images."
-        client.setOutput("Message", rstring(message))
-
+        print("################################")
+        # if none of the keys PARAM_PIXEL_SIZE_Z, PARAM_PIXEL_SIZE_Y or PARAM_PIXEL_SIZE_X are not
+        # provided we can not set the pixel size and therefore stop the script with an error message
+        if not any(k in script_params for k in [PARAM_PIXEL_SIZE_Z, PARAM_PIXEL_SIZE_Y, PARAM_PIXEL_SIZE_X]):
+            client.setOutput("Message", rstring("No pixel size value provided. "
+                                                "Please provide at least one pixel size value."))
+        else:
+            numberOfImages, counter = set_pixel_value(conn, script_params)
+            if counter == 0:
+                message = f"No new pixel size values were set. All {numberOfImages} Image(s) already had pixel size information."
+            else:
+                message = f"Set new pixel size(s) for {counter} of {numberOfImages} Image(s)."
+            client.setOutput("Message", rstring(message))
+        print("###############DONE#############")
+        print("################################")
     finally:
         client.closeSession()
 
